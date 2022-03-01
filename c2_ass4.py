f10bde9765c0749c532f1e76d62e7e428abc1c47
heather-tang <86471943+heather-tang@users.noreply.github.com>
2022-03-02 00:57:46 +0800

    Create c2_ass4.py

---
+++ c2_ass4.py
@@ -0,0 +1,38 @@
+def int_return(x):
+    return x
+  
+  
+  
+def add(x):
+    y = x + 2
+    return y
+
+  
+  
+def change(str):
+    return str + 'Nice to meet you!'
+
+  
+  
+def accum(lst):
+    sum = 0
+    for i in lst:
+        sum += i
+    return sum 
+  
+  
+def length(list):
+  if len(list) > 5 or len(list) == 5:
+      return 'Longer than 5'
+  else:
+      return 'Less than 5'
+
+    
+    
+def divide(x):
+    y = x/2
+    return y 
+
+def sum(x):
+    y = divide(x) + 6
+    return y 
